7524fbc3907d1849622e6ed7e111c31f8bb9d42c
alexander <hsa.lapaz@gmail.com>
2023-05-17 18:20:19 -0600

    Update card-dashboard.blade.php

--- resources/views/components/card-dashboard.blade.php
+++ resources/views/components/card-dashboard.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col text-center shadow-sm m-2 p-2 w-2/5 h-2/5 bg-slate-50 justify-around rounded-lg">
+<div class="flex flex-col text-center shadow-lg m-2 p-2 w-2/5 h-2/5 bg-slate-50 justify-around rounded-lg">
     <div>
     <h1 class="font-bold">{{ $title }}</h1>
     <p class="">{{ $description }}</p>
