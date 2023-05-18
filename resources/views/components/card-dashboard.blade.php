e78f584da92e14455de49c5f41651d14ea96474a
alexander <hsa.lapaz@gmail.com>
2023-05-17 18:19:10 -0600

    Cards done

--- resources/views/components/card-dashboard.blade.php
+++ resources/views/components/card-dashboard.blade.php
@@ -1,5 +1,7 @@
-<div class="flex flex-col">
-    <h1>{{ $title }}</h1>
-    <i class="{{ $image }}"></i>
-    <p>{{ $description }}</p>
+<div class="flex flex-col text-center shadow-sm m-2 p-2 w-2/5 h-2/5 bg-slate-50 justify-around rounded-lg">
+    <div>
+    <h1 class="font-bold">{{ $title }}</h1>
+    <p class="">{{ $description }}</p>
+    </div>
+    <i class="{{ $image }} text-7xl"></i>
 </div>
