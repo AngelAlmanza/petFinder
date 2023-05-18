580058d839d92752885fda1302e233b86cdeab89
Angel Almanza <almanza.angel245@gmail.com>
2023-05-17 17:52:54 -0700

    DashboardController created, routes for dashboard created, base views and files created

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\StorePost;
 use App\Http\Requests\UpdatePost;
 use App\Models\Pet;
 use App\Models\Post;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PetController;
@@ -35,14 +36,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/edit-report/{report}', [ReportController::class, 'edit'])->name('report.edit');
 });
 
-Route::get('/admin/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/admin/dashboard/general-view', [DashboardController::class, 'generalView'])->name('dashboard.general');
+    Route::get('/admin/dashboard/storage', [DashboardController::class, 'storageView'])->name('dashboard.storage');
+    Route::get('/admin/dashboard/lost-pets', [DashboardController::class, 'lostPetsView'])->name('dashboard.lostPets');
+    Route::get('/admin/dashboard/pets-founded', [DashboardController::class, 'petsFoundedView'])->name('dashboard.petsFounded');
+    Route::get('/admin/dashboard/adopted-pets', [DashboardController::class, 'adoptedPetsView'])->name('dashboard.adoptedPets');
 });
 
 require __DIR__.'/auth.php';
