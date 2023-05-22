4385716f07c6e6791e3b6611f36d0c9e3d2740be
Angel Almanza <almanza.angel245@gmail.com>
2023-05-21 20:26:15 -0700

    Added current state field to pets table

--- database/migrations/2023_04_05_184810_create_pets_table.php
+++ database/migrations/2023_04_05_184810_create_pets_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -22,6 +23,7 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('location')->nullable();
             $table->string('url_image');
+            $table->enum('current_state', ['Perdido', 'En adopción', 'Encontrado', 'Adoptado'])->nullable();
             $table->timestamps();
         });
     }
