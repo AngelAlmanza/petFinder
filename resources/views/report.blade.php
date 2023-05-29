239a65829124015cbad0be153db6773d4ee1b8a6
Angel Almanza <almanza.angel245@gmail.com>
2023-05-29 16:44:18 -0700

    Validation for autor and admin before edit or delete

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.plantilla')
 @section('title', 'Reportar')
 @section('content')
+    @php
+    $id = auth()->id();
+    @endphp
     <section class="w-full p-4">
         <h1 class="font-bold text-xl text-neutral-900 text-start mb-4">Motivo del reporte: {{ $report->reason }}</h1>
         <figure>
@@ -16,19 +19,30 @@
         <br>
         <p class="text-base font-normal text-stone-800 mb-4">Reporte realizado por: <i class="text-sm font-light text-gray-400">{{ $report->user->name }}</i></p>
         <p class="text-base font-normal text-stone-800 mb-4">ID del post: <i class="text-sm font-light text-gray-400">{{ $report->post_id }}</i></p>
-        <form action="{{ route('report.destroy', $report) }}" method="POST">
-            @csrf
-            @method('delete')
-            <button type="submit" class="text-base font-normal text-red-800 mb-4">Eliminar reporte</button>
-        </form>
+        @if ($id == $report->user_id)
+            <form action="{{ route('report.destroy', $report) }}" method="POST">
+                @csrf
+                @method('delete')
+                <button type="submit" class="text-base font-normal text-red-800 mb-4">Eliminar reporte</button>
+            </form>
+        @endif
+        @role('admin')
+            <form action="{{ route('report.destroy', $report) }}" method="POST">
+                @csrf
+                @method('delete')
+                <button type="submit" class="text-base font-normal text-red-800 mb-4">Eliminar reporte</button>
+            </form>
+        @endrole
         <div class="mx-auto pb-4 flex justify-center">
             <a href="{{ route('post.index') }}" class="text-slate-100 inline-flex items-center px-4 py-2 border border-transparent rounded-md font-bold text-base uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 bg-red-700">Volver</a>
-            <form action="{{ route('report.edit', $report) }}" method="GET">
-                @csrf
-                <x-primary-button class="ml-4">
-                    {{__('Editar')}}
-                </x-primary-button>
-            </form>
+            @if ($id == $report->user_id)
+                <form action="{{ route('report.edit', $report) }}" method="GET">
+                    @csrf
+                    <x-primary-button class="ml-4">
+                        {{__('Editar')}}
+                    </x-primary-button>
+                </form>
+            @endif
         </div>
     </section>
 @endsection
